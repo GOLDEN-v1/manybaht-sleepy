ae1613bfd422139dd57d303aff7320cfb04cfc4b
manybaht <nantapon.sookpeamron@gmail.com>
2020-08-09 23:59:24 +0700

    add nick.js lol idk

---
+++ commands/nick.js
@@ -0,0 +1,17 @@
+exports.run = async (client, message, args) => {
+	
+    if (!message.member.hasPermission(['ADMINISTRATOR'])) {
+			return message.channel.send("คุณไม่มีอำนาจในการเปลี่ยนชื่อเล่น");
+	}
+	
+	let membermention = message.mentions.members.first();
+	if (!membermention) return message.channel.send("ระบุคนที่ต้องการเปลี่ยนชื่อเล่น");
+	
+	if (!(args[1])) {
+            return message.channel.send('กรุณาระบุชื่อเล่นที่ต้องการเปลี่ยน');
+    }
+	
+	membermention.setNickname(`${args[1]}`);
+	message.channel.send("เปลี่ยนชื่อเล่นเรียบร้อย");
+	
+}
