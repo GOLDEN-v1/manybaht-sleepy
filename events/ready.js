eae59099ebb40fde7b9151e19a6d55dfea19a59a
manybaht <nantapon.sookpeamron@gmail.com>
2020-08-09 21:53:46 +0700

    yeet with status interval

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,7 @@
 module.exports = (client) => {
-  client.user.setPresence({ activity: { name: `${client.users.cache.size} คนน่ารัก` , type: 'WATCHING'}, status: 'online' })
+  client.user.setPresence({ activity: { name: `${client.users.cache.size} คนน่ารัก` , type: 'WATCHING'}, status: 'online' });
+  client.setInterval(() => {
+        client.user.setPresence({ activity: { name: `${client.users.cache.size} คนน่ารัก` , type: 'WATCHING'}, status: 'online' });
+  }, 60000)
   console.log(`Logged In !`);
 }
